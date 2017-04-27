ab86a81ffc3e7e7fe6df34b3cc7a432e4b5952ab
bcv <sogun3@gmail.com>
2017-04-26 21:49:03 -0400

    fizzbuzz error!

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,1 +1,14 @@
-
+int printf();
+int i;
+int main(){
+	for (i = 0 ; i < 100; i ++){
+		if (i % 15 == 0){
+			printf("%d is divisble by 15", i);
+		}else if (i % 3 == 0){
+			printf("%d is divisible by 3\n", i);
+		}else if (i % 5 == 0){
+			printf("%d is divisible by 5\n", i);
+		}
+	}
+	return 0;
+}
